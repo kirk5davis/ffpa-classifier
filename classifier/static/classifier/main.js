550d6e7c52a9bd689aab5a017fd6f3197b62c3b5
Davis <kdav490@dnr.wa.lcl>
2019-01-29 14:04:05 -0800

    added barba.js for transitions

--- classifier/static/classifier/main.js
+++ classifier/static/classifier/main.js
@@ -1,1 +1,28 @@
 //main.js - some simple javascript used for the image classification site
+'use strict';
+
+$('document').ready(function () {
+  var transEffect = Barba.BaseTransition.extend({
+    start: function start() {
+      var _this2 = this;
+
+      this.newContainerLoading.then(function (val) {
+        return _this2.fadeInNewcontent(jQuery(_this2.newContainer));
+      });
+    },
+    fadeInNewcontent: function fadeInNewcontent(nc) {
+      nc.hide();
+      var _this = this;
+      $(this.oldContainer).fadeOut(300).promise().done(function () {
+        nc.css('visibility', 'visible');
+        nc.fadeIn(300, function () {
+          _this.done();
+        });
+      });
+    }
+  });
+  Barba.Pjax.getTransition = function () {
+    return transEffect;
+  };
+  Barba.Pjax.start();
+});
