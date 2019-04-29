b782cfe1138c7d96ac727a4b158d9a212713cdf6
Davis <kdav490@dnr.wa.gov>
2019-04-29 15:19:47 -0700

    added westside and eastside as model type

--- classifier/models.py
+++ classifier/models.py
@@ -5,13 +5,18 @@ class Img(models.Model):
 
     CLASSIFICATION_TYPES = (
         ('ASSESSED','assessed FFPA'),
-        ('PARTIAL','partial FFPA'),
         ('NOT_ASSESSED', 'not assessed FFPA'),
-        ('UNKNOWN', 'unknown'),
+        ('REVIEW_NEEDED', 'review needed'),
+    )
+
+    WA_LANDSCAPE_MODEL_TYPE = (
+        ('WESTSIDE', 'westside model'),
+        ('EASTSIDE', 'eastside model')
     )
 
     image = models.ImageField(upload_to='pics_to_classify/')
     image_classification = models.CharField(max_length=12, choices=CLASSIFICATION_TYPES, blank=True)
+    model_type = models.CharField(max_length=8, choices=WA_LANDSCAPE_MODEL_TYPE, blank=True)
     updated_by_user = models.CharField(max_length=50, blank=True)
 
     # for django shell, upload files:
@@ -20,8 +25,16 @@ class Img(models.Model):
     # ...     pic.image.save(os.path.basename(i), open(i, 'rb'))
 
     @property
-    def pictures_to_classify(self):
+    def pictures_to_classify_all(self):
         return [i for i in Img.objects.all() if not i.image_classification]
+
+    @property
+    def pictures_to_classify_west(self):
+        return [i for i in Img.objects.all() if not i.image_classification and i.model_type == 'WESTSIDE']
+    
+    @property
+    def pictures_to_classify_east(self):
+        return [i for i in Img.objects.all() if not i.image_classification and i.model_type == 'EASTSIDE']
 
     def __str__(self):
         if self.image_classification:
